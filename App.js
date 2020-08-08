b6763e05095edd05c86a9d07a09741105d60ce28
LenaSim <radical.rin@gmail.com>
2020-08-08 14:02:17 +0200

    Main Page UI design

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.title}>Whot To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To Do"}>
+
+        </TextInput>
+      </View>
     </View>
   );
 }
@@ -14,8 +21,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fdd835',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: '#455a64',
+    fontSize: 30,
+    marginVertical: 30,
+    fontWeight: '200'
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    //shadow
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgba(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOrrset: {
+          height: -10,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    }),
+  }
 });
